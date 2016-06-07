6d62dd1095cdcba80aae25a8720d48a344903710
Tom McLean <tom.a.mclean@gmail.com>
2016-06-07 14:36:28 +1000

    Hook it all up to twitter

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
+var log = require('pretty-good-log')('main');
+var config = require('config');
 
-var getExcuses = require('./lib/hansard.js');
+var getExcuses = require('./lib/hansard');
+var store = require('./lib/store');
 
-function main() {
-    getExcuses().then(function(allexcuses) {
-        console.log(allexcuses.map(function(m) {
-            return m.tweet;
-        }));
-    }).catch(function(e) {
-        console.log('error', e);
+var HOUR = 1000 * 60 * 60;
+var moment = require('moment');
+
+var Twitterbot = require('mcccclean-twitterbot');
+var bot = new Twitterbot(config.get('twitterbot'));
+
+function tweet() {
+    store.getTopTweet().then(function(doc) {
+        if(doc) {
+            bot.tweet(doc.tweet);
+            store.flag(doc.gid);
+        } else {
+            log("didn't tweet");
+        }
+    }).catch(function(err) {
+        log('error', err);
     });
 }
 
-main();
+function downloadNewExcuses() {
+    getExcuses().then(function(allexcuses) {
+        allexcuses.map(function(m) {
+            store.addTweet(m);
+        });
+    }).catch(function(e) {
+        log('error', e);
+    });
+}
+
+downloadNewExcuses();
+tweet();
+setInterval(downloadNewExcuses, HOUR * 24);
+setInterval(tweet, HOUR * 4);
