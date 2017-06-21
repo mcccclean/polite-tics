ed17a1364164c37cc10b384a0e074ddeb4e9c783
Tom McLean <tom.a.mclean@gmail.com>
2017-06-22 08:29:11 +1000

    Use scheduler instead of setInterval

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 var log = require('pretty-good-log')('main');
 var config = require('config');
+var schedule = require('node-schedule');
 
 var getExcuses = require('./lib/hansard');
 var store = require('./lib/store');
-
-var HOUR = 1000 * 60 * 60;
-var moment = require('moment');
 
 var Twitterbot = require('mcccclean-twitterbot');
 var bot = new Twitterbot(config.get('twitterbot'));
 
 function tweet() {
-    var h = moment().hour();
-    // keep to tweeting between 8am and 6pm
-    if(h >= 8 && h <= 17) {
-        store.getTopTweet().then(function(doc) {
-            if(doc) {
-                bot.tweet(doc.tweet);
-                store.flag(doc._id);
-                log("Tweeting:", doc.tweet);
-            } else {
-                log("didn't tweet");
-            }
-        }).catch(function(err) {
-            log('error', err);
-        });
-    }
+    return store.getTopTweet().then(function(doc) {
+        if(doc) {
+            bot.tweet(doc.tweet);
+            store.flag(doc._id);
+            log("Tweeting:", doc.tweet);
+        } else {
+            log("didn't tweet");
+        }
+    }).catch(function(err) {
+        log('error', err);
+    });
 }
 
 function downloadNewExcuses() {
-    getExcuses().then(function(allexcuses) {
+    return getExcuses().then(function(allexcuses) {
         allexcuses.map(function(m) {
             store.addTweet(m);
         });
@@ -38,7 +32,9 @@ function downloadNewExcuses() {
     });
 }
 
-downloadNewExcuses();
-tweet();
-setInterval(downloadNewExcuses, HOUR * 24);
-setInterval(tweet, HOUR * 2);
+// download new excuses every 24 hours
+schedule.scheduleJob('0 0 0 * * *', downloadNewExcuses);
+
+// tweet every 2 hours between 8am and 6pm
+schedule.scheduleJob('0 0 8-18/2 * * *', tweet);
+
